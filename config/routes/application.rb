20d743225a7d883434546bbf647720e48015f9ff
gilcierweb <gilcier06@yahoo.com.br>
2024-03-24 20:02:14 -0300

    add controller #home

--- config/routes/application.rb
+++ config/routes/application.rb
@@ -1,5 +1,5 @@
 devise_for :users
- 
+resources :home, only: [:index] 
 
 # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
 # Can be used by load balancers and uptime monitors to verify that the app is live.
