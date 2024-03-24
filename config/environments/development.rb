57d711b552e5063394caa0475a21c20d064d8457
gilcierweb <gilcier06@yahoo.com.br>
2024-03-24 19:58:12 -0300

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  
+
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = true
@@ -50,6 +50,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
